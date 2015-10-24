1df206f36e61b5752e3912ad807fc9a98a00797e
Jonathan Hartman <j@p4nt5.com>
2015-10-23 20:36:11 -0700

    Initial commit; a freshly generated cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Knock Cookbook CHANGELOG
+========================
+
+v0.0.1 (2015-10-23)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'knock'
+maintainer       'Jonathan Hartman'
+maintainer_email 'j@hartman.io'
+license          'apache2'
+description      'Installs/Configures knock'
+long_description 'Installs/Configures knock'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
