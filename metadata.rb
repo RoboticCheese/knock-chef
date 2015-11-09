8ebf64f24c60ddc2bd26286962826f48ac3aab4b
Jonathan Hartman <j@p4nt5.com>
2015-11-08 22:37:25 -0800

    Bump version for dev

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ maintainer_email 'j@hartman.io'
 license          'apache2'
 description      'Installs/configures Knock'
 long_description 'Installs/configures Knock'
-version          '0.1.0'
+version          '0.1.1'
 
 supports         'mac_os_x'
 # rubocop:enable SingleSpaceBeforeFirstArg
